4b062441f9a2ba5e65b19635ace15c4a34216fdf
qusaisabri <qusai.sabri19@gmail.com>
2023-06-12 23:04:32 -0500

    Initial commit

---
+++ data-sense-blazor/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace data_sense_blazor;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ data-sense-blazor/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using data_sense_blazor.Data;
+
+namespace data_sense_blazor;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Logging.AddDebug();
+#endif
+
+		builder.Services.AddSingleton<WeatherForecastService>();
+
+		return builder.Build();
+	}
+}
+
