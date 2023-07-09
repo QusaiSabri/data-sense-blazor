d66ddcb4f490ba759a1bf35961746364171db3c8
Qusai Sabri <qusai.sabri19@gmail.com>
2023-07-09 11:52:45 -0500

    Add "Preview Selected" functionality

--- data-sense-blazor/Shared/AppState.cs
+++ data-sense-blazor/Shared/AppState.cs
@@ -1,16 +1,21 @@
 ﻿using System.Data;
+using data_sense_blazor.Models;
 
 namespace data_sense_blazor.Shared
 {
     public class AppState
     {
         public string Query { get; private set; } = String.Empty;
-
         public DataTable QueryResult { get; private set; } = new DataTable();
+        public List<string> ColumnsSelected { get; private set; } = new List<string>();
+        public Table SelectedTable { get; private set; } = new Table();
+        public string SelectedDatabase { get; private set; } = String.Empty;
 
         public event Action OnQueryResultChange;
-
         public event Action OnQueryChange;
+        public event Action OnColumnsSelectedChange;
+        public event Action OnSelectedTableChange;
+        public event Action OnSelectedDatabaseChange;
 
         public void SetQueryResult(DataTable queryResult)
         {
@@ -22,6 +27,24 @@ namespace data_sense_blazor.Shared
         {
             Query = query;
             OnQueryChange?.Invoke();
+        }
+
+        public void SetColumnsSelected(List<string> columnsSelected)
+        {
+            ColumnsSelected = columnsSelected;
+            OnColumnsSelectedChange?.Invoke();
+        }
+
+        public void SetSelectedTable(Table selectedTable)
+        {
+            SelectedTable = selectedTable;
+            OnSelectedTableChange?.Invoke();
+        }
+
+        public void SetSelectedDatabase(string selectedDatabase)
+        {
+            SelectedDatabase = selectedDatabase;
+            OnSelectedDatabaseChange?.Invoke();
         }
 
     }
